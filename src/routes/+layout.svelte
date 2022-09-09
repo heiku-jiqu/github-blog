873a4b73334a9b8651d7e308e0a3432b3655af52
Heiser <heiser9295@hotmail.com>
2022-09-09 22:56:22 +0800

    change global styles to style.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,8 @@
 <script>
 	import { base } from '$app/paths';
 	import Header from './Header.svelte';
+	import '../style.css';
 </script>
 
 <Header />
 <slot />
-
-<style global>
-	/* CSS HEX 
---eggshell: #f4f1deff;
---terra-cotta: #e07a5fff;
---independence: #3d405bff;
---green-sheen: #81b29aff;
---deep-champagne: #f2cc8fff;
-*/
-	body {
-		margin: 0;
-		background: #f4f1deff;
-		color: #3d405bff;
-		font-family: Monaco, monospace;
-		text-align: center;
-	}
-
-	a:link {
-		color: #3d405bff;
-	}
-	a:hover {
-		color: #f2cc8fff;
-	}
-	a:visited {
-		color: #81b29aff;
-	}
-	main {
-		padding: 1rem;
-		margin: 2rem auto;
-		max-width: 40rem;
-	}
-</style>
