22e151b9da1d2dda9f8e8062eaea559529b8d5fd
Heiser <heiser9295@hotmail.com>
2022-09-09 14:59:02 +0800

    add preprocess and mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
+import { msdvex } from 'mdsvex';
+import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	extensions: ['.svelte', '.svx'],
+	preprocess: [sveltePreprocess(), msdvex()]
 };
 
 export default config;
