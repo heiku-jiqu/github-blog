08fb7084f2e5dca04da4e19bbf1917244fe81534
Heiser <heiser9295@hotmail.com>
2022-09-14 20:52:56 +0800

    edit article layout

--- src/routes/blog/[blogpost]/+page.svelte
+++ src/routes/blog/[blogpost]/+page.svelte
@@ -3,4 +3,23 @@
 	//console.log(data);
 </script>
 
-<svelte:component this={data.content} />
+<div class="grid-container">
+	<div class="sides" />
+	<div class="article">
+		<svelte:component this={data.content} />
+	</div>
+	<div class="sides" />
+</div>
+
+<style>
+	.grid-container {
+		display: grid;
+		grid-template-columns: 1fr min(100%, 90ch) 1fr;
+	}
+	.article {
+		padding: 2rem;
+		text-align: justify;
+	}
+	code {
+	}
+</style>
