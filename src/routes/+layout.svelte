b7ca44831b071b6838edd1dd29b9ab1fb1157024
heiser <e0031022@u.nus.edu>
2022-09-19 14:41:19 +0800

    add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script>
+	export let data;
+
 	import { base } from '$app/paths';
 	import Header from './Header.svelte';
 	import '../style.css';
+
+	import { fade } from 'svelte/transition';
 </script>
 
 <Header />
-<slot />
+{#key data.currentRoute}
+	<div in:fade>
+		<slot />
+	</div>
+{/key}
