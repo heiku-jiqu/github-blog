91959ad881c953e8c1fcb64205342cc38f64276b
Heiser <heiser9295@hotmail.com>
2022-10-13 20:43:40 +0800

    refactor data fetch

--- src/routes/blog/components/GovtBalance.svelte
+++ src/routes/blog/components/GovtBalance.svelte
@@ -1,34 +1,33 @@
 <script>
-	import { onMount } from 'svelte';
-	import Carousell from './Carousell.svelte';
+	import { xlink_attr } from 'svelte/internal';
 
-	console.log('hello');
-	const data_gov_url = 'https://data.gov.sg/api/action/datastore_search';
-	const resource_id = '98856a60-33cd-482a-9dc4-1ed52e562d5d';
-	const params_obj = {
-		resource_id: resource_id,
-		q: 'balance'
-	};
+	async function fetch_govt_balance_data() {
+		const data_gov_url = 'https://data.gov.sg/api/action/datastore_search';
+		const resource_id = '98856a60-33cd-482a-9dc4-1ed52e562d5d';
+		const params_obj = {
+			resource_id,
+			q: 'balance'
+		};
+		let url_search_params = new URLSearchParams(params_obj);
 
-	let url_search_params = new URLSearchParams(params_obj);
-	let govt_balance_data;
-	onMount(async () => {
-		await fetch(data_gov_url + '?' + url_search_params)
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-				govt_balance_data = data.result.records;
-				console.log(govt_balance_data);
-			});
-	});
+		const response = await fetch(data_gov_url + '?' + url_search_params);
+		const response_json = await response.json();
+		console.log(response_json);
+		return response_json.result.records;
+	}
+
+	let govt_balance_data_promise = fetch_govt_balance_data();
 </script>
 
 <div>
-	{#if govt_balance_data}
-		{#each govt_balance_data as row}
-			<section>{row.amount}</section>
-		{/each}
-	{:else}
+	{#await govt_balance_data_promise}
 		<p>loading..</p>
-	{/if}
+	{:then govt_balance_data}
+		{#each govt_balance_data as r}
+			<section>
+				Year: {r.year_of_balance} | Balance: {r.amount} | Type: {r.actual_revised_estimated} | Item:
+				{r.item}
+			</section>
+		{/each}
+	{/await}
 </div>
