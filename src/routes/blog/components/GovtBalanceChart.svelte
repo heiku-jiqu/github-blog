a997beb829d77844a6f8c9b858c94d29de27ccee
Heiser <heiser9295@hotmail.com>
2022-11-03 11:46:48 +0800

    use scaleband

--- src/routes/blog/components/GovtBalanceChart.svelte
+++ src/routes/blog/components/GovtBalanceChart.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { loop_guard, xlink_attr } from 'svelte/internal';
 	import { fade } from 'svelte/transition';
-	import { scaleLinear } from 'd3-scale';
+	import { scaleLinear, scaleBand } from 'd3-scale';
 
 	async function fetch_govt_balance_data() {
 		const data_gov_url = 'https://data.gov.sg/api/action/datastore_search';
@@ -24,14 +24,18 @@
 
 	let plotMargin = 100;
 	let chartHeight = 600;
+	let chartWidth = 800;
 	let yScale = scaleLinear();
+	let xScale = scaleBand();
 
 	let rect_hover = null;
 
 	let govt_balance_data_promise = fetch_govt_balance_data();
 	let filtered_data_promise = filter_data(govt_balance_data_promise).then((data) => {
 		let yDomain = data.map((r) => r.amount);
+		let xDomain = data.map((r) => r.year_of_balance);
 		yScale.domain([Math.min(...yDomain), Math.max(...yDomain)]).range([chartHeight, 0]);
+		xScale.domain(xDomain).range([0, chartWidth]).paddingInner(0.1);
 		rect_hover = yDomain.map(() => false);
 		console.log(Math.max(...yDomain));
 		console.log(data.map((r) => yScale(r.amount)));
@@ -42,15 +46,15 @@
 <p>{rect_hover}</p>
 {#await filtered_data_promise then filtered_data}
 	<svg
-		viewBox="{0 - plotMargin} {0 - plotMargin} {filtered_data.length * 50 +
-			2 * plotMargin} {chartHeight + 2 * plotMargin}"
+		viewBox="{0 - plotMargin} {0 - plotMargin} {chartWidth + 2 * plotMargin} {chartHeight +
+			2 * plotMargin}"
 	>
 		{#each filtered_data as r, i}
 			<rect
 				in:fade={{ delay: i * 50 }}
 				y={yScale(Math.max(0, r.amount))}
-				x={i * 50}
-				width="30"
+				x={xScale(r.year_of_balance)}
+				width={xScale.bandwidth()}
 				height={Math.abs(yScale(r.amount) - yScale(0))}
 				on:mouseenter={() => {
 					rect_hover[i] = true;
@@ -64,28 +68,28 @@
 		{#each filtered_data as r, i}
 			{#if rect_hover[i]}
 				<text
-					x={i * 50}
+					x={xScale(r.year_of_balance)}
 					y={r.amount > 0 ? yScale(r.amount) - 10 : yScale(r.amount) + 10}
 					dominant-baseline={r.amount > 0 ? 'baseline' : 'hanging'}
 				>
 					Year: {r.year_of_balance}
 				</text>
 				<text
-					x={i * 50}
+					x={xScale(r.year_of_balance)}
 					y={r.amount > 0 ? yScale(r.amount) - 24 : yScale(r.amount) + 24}
 					dominant-baseline={r.amount > 0 ? 'baseline' : 'hanging'}
 				>
 					Balance: {r.amount}
 				</text>
 				<text
-					x={i * 50}
+					x={xScale(r.year_of_balance)}
 					y={r.amount > 0 ? yScale(r.amount) - 38 : yScale(r.amount) + 38}
 					dominant-baseline={r.amount > 0 ? 'baseline' : 'hanging'}
 				>
 					Type: {r.actual_revised_estimated}
 				</text>
 				<text
-					x={i * 50}
+					x={xScale(r.year_of_balance)}
 					y={r.amount > 0 ? yScale(r.amount) - 52 : yScale(r.amount) + 52}
 					dominant-baseline={r.amount > 0 ? 'baseline' : 'hanging'}
 				>
