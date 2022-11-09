8247bc4d161e1d17e2d72f5d4c5923795a397a40
Heiser <heiser9295@hotmail.com>
2022-11-09 20:50:57 +0800

    use lib fetch function

--- src/routes/blog/components/GovtBalance.svelte
+++ src/routes/blog/components/GovtBalance.svelte
@@ -1,21 +1,7 @@
 <script>
 	import { xlink_attr } from 'svelte/internal';
 	import { fade } from 'svelte/transition';
-
-	async function fetch_govt_balance_data() {
-		const data_gov_url = 'https://data.gov.sg/api/action/datastore_search';
-		const resource_id = '98856a60-33cd-482a-9dc4-1ed52e562d5d';
-		const params_obj = {
-			resource_id,
-			q: 'balance'
-		};
-		let url_search_params = new URLSearchParams(params_obj);
-
-		const response = await fetch(`${data_gov_url}?${url_search_params}`);
-		const response_json = await response.json();
-		// console.log(response_json);
-		return response_json.result.records;
-	}
+	import { fetch_govt_balance_data } from '$lib/datagovsg/GovtBalanceData.js';
 
 	let govt_balance_data_promise = fetch_govt_balance_data();
 </script>
